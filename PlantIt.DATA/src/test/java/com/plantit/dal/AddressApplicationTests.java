4a321c1f49172f58a0871acd4d2f83fa2b4673da
Lucie <desmons.lucie@gmail.com>
2023-02-17 00:00:10 +0100

    [test] test create, findById, update, delete and findAll with Address Entity (ALL OK)

--- PlantIt.DATA/src/test/java/com/plantit/dal/AddressApplicationTests.java
+++ PlantIt.DATA/src/test/java/com/plantit/dal/AddressApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.plantit.dal.model.Address;
 import com.plantit.dal.repository.AddressRepository;
 
+import java.util.List;
+
 
 @SpringBootTest
 class AddressApplicationTests {
@@ -15,8 +17,40 @@ class AddressApplicationTests {
 
 	@Test
 	public void testCreateAddress() {
-		Address address = new Address(230, 62490, "rue de Bellonne", "", "Noyelles-sous-Bellonne");
+		Address address = new Address(1195, 69280, "Avenue Marcel Mérieux", "Bâtiment A, RDC", "Marcy-l'Etoile");
 		addressRepository.save(address);
+	}
+
+	@Test
+	public void testFindAddress()
+	{
+		Address a = addressRepository.findById(1L).get();
+		System.out.println(a);
+	}
+
+	@Test
+	public void testUpdateAddress()
+	{
+		Address a = addressRepository.findById(1L).get();
+		a.setTown("Noyelles");
+		addressRepository.save(a);
+	}
+
+	//Nadhem Bel Hadj- nadhemb@yahoo.com Page 5 Spring Boot 2022
+	@Test
+	public void testDeleteAddress()
+	{
+		addressRepository.deleteById(2L);;
+	}
+
+	@Test
+	public void testListAllAddresses()
+	{
+		List<Address> addresses = addressRepository.findAll();
+		for (Address a : addresses)
+		{
+			System.out.println(a);
+		}
 	}
 
 }
